2022882745c621c69529b5d1668bdec53b4dacc8
Romulo P T Mota <romuloptmota@gmail.com>
2024-12-31 16:32:53 -0300

    Criado Atividade

---
+++ src/main/java/com/devsuperior/sistemaevento/entities/Atividade.java
@@ -0,0 +1,81 @@
+package com.devsuperior.sistemaevento.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    //ATRIBUTOS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private Double preco;
+
+
+    //CONSTRUTORES
+    public Atividade() {
+    }
+
+    public Atividade(Integer id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+
+    //GETTER E SETTER
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+
+    //EQUALS HASHCODE
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Atividade atividade = (Atividade) o;
+        return Objects.equals(id, atividade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
